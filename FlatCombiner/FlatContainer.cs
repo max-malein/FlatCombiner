be20339b68d9f46380db0f1e580304be33824116
Max <max@malein.com>
2018-12-21 16:00:12 +0300

    First commit

---
+++ FlatCombiner/FlatContainer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlatCombiner
+{
+    public class FlatContainer
+    {
+        private string id="";
+
+        public string Id { get { return id; } set
+            {
+                id = value;
+                if (value == null) return;
+                string pat = @".{3}(?=_[UD])";
+                var code = System.Text.RegularExpressions.Regex.Match(id, pat).Value;
+                switch (code)
+                {
+                    case "b_R":
+                        FType = FlatLocattionType.CornerRight;
+                        break;
+
+                    case "b_L":
+                        FType = FlatLocattionType.CornerLeft;
+                        break;
+
+                    default:
+                        FType = FlatLocattionType.Middle;
+                        break;
+                }
+                return;
+            }
+        }
+        public int BottomSteps { get; set; }
+        public int TopSteps { get; set; }
+        public FlatLocattionType FType {get; set;}
+
+        public override string ToString()
+        {
+            return string.Format("{0}, bSteps = {1}, tSteps = {2}, {3}", Id, BottomSteps, TopSteps, FType);
+        }
+
+        public enum FlatLocattionType
+        {
+            CornerLeft,
+            CornerRight,
+            Middle
+        }
+    }
+
+    
+}
+
--- FlatCombiner/Program.cs
+++ FlatCombiner/Program.cs
@@ -3,13 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Robot;
+using Newtonsoft.Json;
+using Rhino;
+using Rhino.Geometry;
+
 
 namespace FlatCombiner
 {
     class Program
     {
         static void Main(string[] args)
-        {
+        {            
+            string json = System.IO.File.ReadAllText(@"D:\Dropbox\WORK\154_ROBOT\04_Grasshopper\Source\flats03.json");
+            var flats = JsonConvert.DeserializeObject<List<FlatContainer>>(json);
+            flats.RemoveAll(item => item == null);
+
+
+
+            foreach (var flat in flats)
+            {                
+                Console.WriteLine(flat.ToString());
+            }
+            Console.ReadKey();
         }
     }
 }
